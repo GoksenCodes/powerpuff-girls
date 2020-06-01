734340c59f000b4a469e8afd58c94508fcf2822c
Goksen <goksengorgulu@gmail.com>
2020-06-01 20:22:40 +0200

    remove showDetails component&simplfy showPage

--- src/pages/ShowPage/index.js
+++ src/pages/ShowPage/index.js
@@ -2,24 +2,32 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchShowDetails } from "../../store/showDetails/action";
 import { selectShowDetails } from "../../store/showDetails/selectors";
-import ShowDetails from "../../components/ShowDetails";
 
 export default function ShowPage() {
   const dispatch = useDispatch();
   const showDetails = useSelector(selectShowDetails);
   console.log("show details at showpage", showDetails);
+  const stripHtml = require("string-strip-html");
+  const description = showDetails.summary
+    ? stripHtml(showDetails.summary)
+    : null;
+
   useEffect(() => {
     dispatch(fetchShowDetails());
   }, [dispatch]);
 
   return (
     <div>
-      <ShowDetails
-        id={showDetails.id}
-        title={showDetails.name}
-        description={showDetails.summary}
-        image={showDetails.image ? showDetails.image.medium : null}
-      />
+      <h1>{showDetails.name}</h1>
+      <div>
+        <div>
+          <img
+            src={showDetails.image ? showDetails.image.medium : null}
+            alt={showDetails.title}
+          />
+        </div>
+        <div>{description}</div>
+      </div>
     </div>
   );
 }
