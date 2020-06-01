12566265d0aa85a08246264dac370a0a315b950a
Goksen <goksengorgulu@gmail.com>
2020-06-01 20:23:43 +0200

    remove nav

--- src/App.js
+++ src/App.js
@@ -2,21 +2,16 @@ import React from "react";
 import "./App.css";
 import { Switch, Route, Link } from "react-router-dom";
 import ShowPage from "./pages/ShowPage";
-
-const Home = () => <h1>Home</h1>;
-const Other = () => <h1>Other</h1>;
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <nav>
-        <Link to={"/"}>Home</Link>
-        <Link to={"/other"}>Other</Link>
-      </nav>
-      <Switch>
-        <Route exact path="/" component={ShowPage} />
-        <Route path="/other" component={Other} />
-      </Switch>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={ShowPage} />
+        </Switch>
+      </Router>
     </div>
   );
 }
