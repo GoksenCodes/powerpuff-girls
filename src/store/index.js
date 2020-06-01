1f910ef7cb5902d9d75bdb7c3b3a1093ca19834f
Goksen <goksengorgulu@gmail.com>
2020-06-01 12:48:32 +0200

    update folder structure

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,8 @@
 import { createStore } from "redux";
-import reducer from "./reducer";
-
-import ReduxThunk from "redux-thunk";
 import reducer from "./rootReducer";
 
-const devTools =
+const enhancer =
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-
-const enhancer = compose(applyMiddleware(ReduxThunk), devTools);
-
 const store = createStore(reducer, enhancer);
 
 export default store;
